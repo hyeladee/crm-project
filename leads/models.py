fc0522a6fd6f7cac20b207ca3c0c3dd7f51752bf
David D <67209076+hyeladee@users.noreply.github.com>
2024-09-06 15:57:34 +0100

    model implementations

--- leads/models.py
+++ leads/models.py
@@ -1,15 +1,37 @@
 from django.db import models
-# from django.contrib.auth.models import AbstractUser
+# from django.contrib.auth import get_user_model # using default django user
+from django.contrib.auth.models import AbstractUser # using custom django user
 
-# # Create your models here.
-# class User(AbstractUser):
-#     pass
+# Create your models here.
+# using custom django user
+class User(AbstractUser):
+    pass
 
 # class Agent(models.Model):
 #     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+# User = get_user_model() # using default django user
+
 class Lead(models.Model):
+    # SOURCE_CHOICES = (
+    #     ('Youtube', 'Youtube'),
+    #     ('Twitter', 'Twitter'),
+    #     ('Facebook', 'Facebook'),
+    # )
+    
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
-    # agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+    agent = models.ForeignKey('Agent', on_delete=models.CASCADE)
+
+    # phoned = models.BooleanField(default=False)   
+    # source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
+    # profile_picture = models.ImageField(blank=True, null=True)
+    # special_files = models.FileField(blank=True, null=True)
+
+    # agent = models.ForeignKey('Agent', on_delete=models.SET_DEFAULT, null=True, default=1) #assuming category 1 is default eg 'no_agent_assigned', or some agent selected randomly or criteria based 
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # first_name = models.CharField(max_length=20)
+    # last_name = models.CharField(max_length=20)
