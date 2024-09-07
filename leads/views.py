c55dee788d127901c215bc9c91b69b3ef6464bd2
David D <67209076+hyeladee@users.noreply.github.com>
2024-09-07 10:33:03 +0100

    added lead_list and lead_detail pages

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Lead
 
 # Create your views here.
+def lead_list(request):
+    leads = Lead.objects.all()
+    context = {
+        'leads': leads,
+    }
+    return render(request, 'leads/lead_list.html', context)
+
+    # return HttpResponse('Hello World')
+    # context = {
+    #     'name': 'Joe',
+    #     'age': 35,
+    # }
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        'lead': lead
+    }
+    return render(request, 'leads/lead_detail.html', context)
+    
+    # return HttpResponse('Here is the detail view')
