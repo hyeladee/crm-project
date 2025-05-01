4714a4978e71c5cf0a66fd74fa80f1e24d7ae9e0
hyeladee <dave4digits@gmail.com>
2025-05-01 09:30:32 +0100

    minor fix

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
-        ssl_require=not DEBUG,
+        # ssl_require=not DEBUG,
     )
 }
 
